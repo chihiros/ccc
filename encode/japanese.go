b12924c1f0efdc9b8f55971d5806a14ae8dc2d30
SuzurikawaChihiro <chihiro.suzurikawa@kddi-web.com>
2022-03-22 09:58:01 +0900

    add: EUC-JPとISO-2022-JPの対応

--- encode/japanese.go
+++ encode/japanese.go
@@ -7,7 +7,12 @@ func (e *Encode) ShiftJIS() ([]byte, error) {
 	return b, err
 }
 
-func (e *Encode) Windows31J() ([]byte, error) {
-	b, err := encodeReader(strings.NewReader(e.Target), "Windows-31J")
+func (e *Encode) EUC_JP() ([]byte, error) {
+	b, err := encodeReader(strings.NewReader(e.Target), "euc-jp")
+	return b, err
+}
+
+func (e *Encode) ISO_2022_JP() ([]byte, error) {
+	b, err := encodeReader(strings.NewReader(e.Target), "iso-2022-jp")
 	return b, err
 }
